0c4f37f8e66fafcb4b6c7b2de2b806cb557e0656
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-04 21:35:07 +0300

    Added Terms and Conditions and Privacy Policy links to constants.js

---
+++ src/constants/links.js
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export const TERMS_AND_CONDITIONS = 'https://www.freeprivacypolicy.com/live/18f24db5-84fe-443e-8076-82d5fae16dba';
+export const PRIVACY_POLICY_LINK = 'https://www.freeprivacypolicy.com/live/2e341c77-cf33-4dd3-89b6-5dc351e2646a'
