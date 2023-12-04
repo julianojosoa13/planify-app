5a43c8c83ef84769faa492e8f1a99fbdf76665e5
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-04 21:53:00 +0300

    Terms & Conditions and Privacy Policy link redirection

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { View, Text } from 'react-native';
+import { View, Text, Linking } from 'react-native';
 import React,  {useState} from 'react';
 
 import styles from './styles';
@@ -7,13 +7,18 @@ import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 import Title from '../../../components/Title';
 import CheckBox from '../../../components/CheckBox';
+import { PRIVACY_POLICY_LINK, TERMS_AND_CONDITIONS } from '../../../constants/links';
 
 function Signup({navigation}) {
   const [agree, setAgree] = useState(false)
 
   const onCheckBoxPress = () => {
     setAgree( value=> !value )
-  } 
+  }
+
+  const onLinkPress = (url) => {
+    Linking.openURL(url)
+  }
   return (
     <View style={styles.container}>
       <Title>Join the hub!</Title>
@@ -33,9 +38,9 @@ function Signup({navigation}) {
         <CheckBox checked={agree} onPress={onCheckBoxPress} />
         <Text style={styles.tosText}>
           I agree to{` `}
-          <Text style={styles.link}>Terms and Conditions </Text>
+          <Text style={styles.link} onPress={() => onLinkPress(TERMS_AND_CONDITIONS)}>Terms and Conditions </Text>
           and{` `}
-          <Text style={styles.link}>Privacy Policy</Text> 
+          <Text style={styles.link} onPress={() => onLinkPress(PRIVACY_POLICY_LINK)}>Privacy Policy</Text> 
         </Text>
       </View>
     </View>
