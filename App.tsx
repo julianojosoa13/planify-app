319f86596b0790b78a0e2eb773e0d041144d55e1
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-07 22:58:57 +0300

    Added custom theme in App.tsx

--- App.tsx
+++ App.tsx
@@ -8,13 +8,21 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 
 import Routes from './src/Routes';
 
 function App(): JSX.Element {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#ffffff',
+    }
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <Routes />
     </NavigationContainer>
   );
