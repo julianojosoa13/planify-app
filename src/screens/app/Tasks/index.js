cfa1c8b32183ef3b489367bd4b63b84b7d78b23d
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-12-20 22:22:11 +0300

    Displaying tasks list from Redux

--- src/screens/app/Tasks/index.js
+++ src/screens/app/Tasks/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { View, Text, Image, SafeAreaView, ScrollView } from 'react-native';
+import { View, Text, Image, SafeAreaView, ScrollView, FlatList } from 'react-native';
 import React from 'react';
 
 import styles from './styles';
@@ -7,16 +7,31 @@ import Title from '../../../components/Title';
 import Button from '../../../components/Button';
 import Header from '../../../components/Header';
 import PlusIcon from '../../../components/PlusIcon';
+import { useSelector } from 'react-redux';
+import CheckBox from '../../../components/CheckBox';
 
 function Tasks() {
+  const tasks = useSelector(state => state.tasks.data)
+
+  const renderTasks = (item) => {
+    return (
+      <View style={styles.row}>
+        <CheckBox checked={item.checked} />
+        <Text style={styles.taskText}>{item.title}</Text>
+      </View>
+    )
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <Header title={'Tasks'}/>
       
-      <ScrollView>
-        <Title type='thin'>To Do Tasks:</Title>
-      </ScrollView>
+      <FlatList 
+        data={tasks}
+        keyExtractor={(item) => String(item.uid)}
+        renderItem={({item}) => renderTasks(item)}
+        ListHeaderComponent={<Title type='thin'>To Do Tasks:</Title>}
+      />
 
       <PlusIcon />
     </SafeAreaView>
--- src/screens/app/Tasks/styles.js
+++ src/screens/app/Tasks/styles.js
@@ -8,6 +8,16 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
     },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 24,
+        marginVertical: 10
+    },
+    taskText: {
+        color: colors.black,
+        marginLeft: 8
+    }
 });
 
 export default styles;
